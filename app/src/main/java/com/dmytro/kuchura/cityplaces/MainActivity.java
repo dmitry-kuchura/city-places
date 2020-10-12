11fe6d5fb0c18db4afbe217d0155135058c2fc04
Dmytro Kuchura <dmytro.kuchura@amadeus.com>
2020-10-12 22:27:50 +0300

    add navigation

--- app/src/main/java/com/dmytro/kuchura/cityplaces/MainActivity.java
+++ app/src/main/java/com/dmytro/kuchura/cityplaces/MainActivity.java
@@ -1,7 +1,19 @@
 package com.dmytro.kuchura.cityplaces;
 
+import android.view.View;
+import android.widget.TextView;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.navigation.NavController;
+import androidx.navigation.NavDestination;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
+import com.google.android.material.navigation.NavigationView;
+import org.jetbrains.annotations.NotNull;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +21,33 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final DrawerLayout drawerLayout = findViewById(R.id.drawerLayout);
+
+        findViewById(R.id.imageMenu).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                drawerLayout.openDrawer(GravityCompat.START);
+            }
+        });
+
+        NavigationView navigationView = findViewById(R.id.navigationView);
+        navigationView.setItemIconTintList(null);
+
+        NavController navController = Navigation.findNavController(this, R.id.navHostFragment);
+        NavigationUI.setupWithNavController(navigationView, navController);
+
+        final TextView textTitle = findViewById(R.id.textTitle);
+
+        navController.addOnDestinationChangedListener(new NavController.OnDestinationChangedListener() {
+            @Override
+            public void onDestinationChanged(
+                    @NonNull @NotNull NavController controller,
+                    @NonNull @NotNull NavDestination destination,
+                    @Nullable @org.jetbrains.annotations.Nullable Bundle arguments
+            ) {
+                textTitle.setText(destination.getLabel());
+            }
+        });
     }
 }
