973030fd486d95864c59872bdefdbb60bd23639f
Dmytro Kuchura <dmytro.kuchura@amadeus.com>
2020-10-15 13:56:10 +0300

    add welcome screen

---
+++ app/src/main/java/com/dmytro/kuchura/cityplaces/WelcomeActivity.java
@@ -0,0 +1,28 @@
+package com.dmytro.kuchura.cityplaces;
+
+import android.content.Intent;
+import android.os.Handler;
+
+import androidx.fragment.app.FragmentActivity;
+
+import android.os.Bundle;
+
+public class WelcomeActivity extends FragmentActivity {
+
+    int SPLASH_TIME = 3000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_wellcome);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent mySuperIntent = new Intent(WelcomeActivity.this, MainActivity.class);
+                startActivity(mySuperIntent);
+                finish();
+            }
+        }, SPLASH_TIME);
+    }
+}
